1a7c598238f2699cec6f943cc55413b90c22a71b
Md Samim Akhter <72917445+imdsamimakhter@users.noreply.github.com>
2024-05-03 21:16:09 +0530

    Update index.php

--- simple-php-website-master/index.php
+++ simple-php-website-master/index.php
@@ -1,4 +1,22 @@
-<?php
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta name="description" content="Webpage description goes here" />
+  <meta charset="utf-8">
+  <title>Change_me</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+</head>
+
+<body>
+ <?php
 
 // Comment these lines to hide errors
 error_reporting(E_ALL);
@@ -8,3 +26,7 @@ require 'includes/config.php';
 require 'includes/functions.php';
 
 init();
+?>
+
+</body>
+</html>
